f90967e0bddf3bfa743caa5bd511bbb8b9798436
ramak8sgcp <ramak8sgcp@gmail.com>
2025-02-05 13:33:30 +0530

    terraform

---
+++ 20-sg/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+  # makesure we below S3 and DynamoDB should be there
+  backend "s3" {
+    bucket         = "81s-remote-state-rama"
+    key            = "expense-vpc-dev"
+    region         = "us-east-1"
+    dynamodb_table = "81s-locking"
+
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
