f537b05e676af6ee64b7eefead30971b33050072
ramak8sgcp <ramak8sgcp@gmail.com>
2025-12-01 16:57:45 +0530

    update

--- 20-sg/parameters.tf
+++ 20-sg/parameters.tf
@@ -10,6 +10,7 @@ resource "aws_ssm_parameter" "backend_sg_id" {
   name  = "/${var.project_name}/${var.environment}/backend_sg_id"
   type  = "String"
   value = module.backend_sg.id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "frontend_sg_id" {
@@ -17,6 +18,7 @@ resource "aws_ssm_parameter" "frontend_sg_id" {
   name  = "/${var.project_name}/${var.environment}/frontend_sg_id"
   type  = "String"
   value = module.frontend_sg.id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "bastion_sg_id" {
@@ -31,4 +33,5 @@ resource "aws_ssm_parameter" "ansible_sg_id" {
   name  = "/${var.project_name}/${var.environment}/ansible_sg_id"
   type  = "String"
   value = module.ansible_sg.id
+  overwrite = true
 }
