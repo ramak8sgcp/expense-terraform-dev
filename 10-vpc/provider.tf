c5d694bee4f551c1d358ad97e9402e966e61ad6e
ramak8sgcp <ramak8sgcp@gmail.com>
2025-12-01 19:46:23 +0530

    update

--- 10-vpc/provider.tf
+++ 10-vpc/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.66.0"
+      version = ">= 6.21.0"
     }
   }
   # makesure we below S3 and DynamoDB should be there
